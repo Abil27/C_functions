a67897f18939dbb5b0192f40c169f1f14812ffa8
Ahssimi mamadou Housseini <ahoussei@e1z1r4p30.42.us.org>
2018-04-18 20:20:06 -0700

    adding ft_puchar program

---
+++ ft_putchar.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahoussei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/04/18 18:52:48 by ahoussei          #+#    #+#             */
+/*   Updated: 2018/04/18 20:18:45 by ahoussei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		main(int argc, char **argv)
+{
+	while(argc != 1)
+	{
+		//print the first caracter
+		ft_putchar(argv[1][0]);
+		return (0);
+	}
+}
